50bf1f9a590bfc9cda2ec2d96e2b0ab05e96f9b7
ryan-allan <ryan.allen@skycore.com>
2013-08-11 02:34:35 -0400

    Create testcore.php

---
+++ 1.0/source_code/testcore.php
@@ -0,0 +1,22 @@
+
+//Test Textfield and Button
+<form action="testcore.php" method="get">
+Your number must be of the format 1-XXX-XXX-XXXX (excluding hyphens) :<input type="text" name="number">
+<input type="submit" name="submit" value="Subscribe">
+</form>
+
+
+<?php
+
+include ('skycore_sdk.php');
+
+$key = "NiP1t1WsCPwHz8ujHRLxMtZdA9de8AqU";
+$campid = "1605";
+$number = $_GET["number"];
+
+
+$skycore = new Skycore($key);
+
+$skycore->subscribe($key, $number, $campid);
+
+?>
