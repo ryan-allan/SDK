a38af7568a5a62456c049ef529bd6a1e48360843
ryan-allan <ryan.allen@skycore.com>
2013-08-14 13:57:22 -0400

    Update skycore_sdk.php

--- 1.0/source_code/skycore_sdk.php
+++ 1.0/source_code/skycore_sdk.php
@@ -1,49 +1,40 @@
 <?php
 
-include ("buildurllibrary.inc.php");
+	class Skycore	{
 
-class Skycore	{
-	
-	public function Skycore($key)	{
-	
-		$this->api_key = "api_key=" . $key;
-	
-	}
-	
-	//function for the subscribe call
-	public function subscribe($number, $campid)	{
+		public function Skycore($key)	{
 
-		//URL Query Information
-		//---------------------
-		//Static information
-		$action = "action=subscribe"; 
-		//Dynamic information
-		$mobile = "mobile=";
-		$campaignid = "campaignid=";
-		//Assemble the URL query
-		$api_query = $action . "&" . $this->api_key . "&" . $mobile . $number . "&" . $campaignid . $campid;
-	
-	
-		//Make the HTTPS request
-		//----------------------
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 
-			//Builds the final request URL by concatenating the base URL with the URL query
-			http_build_url('https://scale-secure.skycore.com/API/wxml/1.3/index.php?',
-				array(
-					
-					"query" => $api_query
-				)
-		));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		//Set this to 0 if testing on a dev site
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		$result = curl_exec($ch);
+			$this->api_key = $key;
+
+		}
+
+		//Function for the subscribe call
+		//-------------------------------
+		public function subscribe($number, $campid)	{
+
+			//XML Query Information
+			//---------------------
+			$POSTArray['XML']=
+			"<REQUEST>
+			<ACTION>subscribe</ACTION>
+			<API_KEY>".$this->api_key."</API_KEY>
+			<CAMPAIGNID>".$campid."</CAMPAIGNID>
+			<MOBILE>".$number."</MOBILE>
+			</REQUEST>";
+
+			//Make the HTTPS request
+			//----------------------
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'https://scale-secure.skycore.com/API/wxml/1.3/index.php?');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $POSTArray);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+			$result = curl_exec($ch);
+
+		}
 
 	}
-
-}
 
 ?>
