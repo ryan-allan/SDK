1bad51416142756b6b64d51667405d965b31e81a
ryan-allan <ryan.allen@skycore.com>
2013-08-22 23:19:58 -0400

    Update testcore.php

--- 1.0/source_code/testcore.php
+++ 1.0/source_code/testcore.php
@@ -1,19 +1,88 @@
-//Test Textfield and Button
 <form action="testcore.php" method="get">
-Your number must be of the format 1-XXX-XXX-XXXX (excluding hyphens) :<input type="text" name="number">
-<input type="submit" name="submit" value="Subscribe">
+Your number must be of the format 1-XXX-XXX-XXXX (excluding hyphens) :<input type="text" name="mobile">
+Enter Name:<input type="text" name="name">
+Enter Duration:<input type="text" name="duration">
+Enter Image URL :<input type="text" name="url">
+<input type="submit" name="submit" value="Submit">
 </form>
-
 
 <?php
 
 include ('skycore_sdk.php');
+//-------Dev Key-------
+$key = "KvP46UOdREGTOWNRvEHUE8cc9I2v1SuL";
 
-$key = ""; //Enter a working key here.
-$campid = "1605";
-$number = $_GET["number"];
+//-------Simple Test-------
 
+/*
+$request = array(
+	'action'	 => 'subscribe',
+	'campaignid' => '1605',
+	'mobile'     => $_GET["mobile"]
+);
+*/
+
+//-------Multidimensional Test-------
+/*
+$request = array(
+	'action' => 'sendPassInEmail',
+	'emailid' => '42876',
+	'email' => 'ryan.allen@skycore.com',
+	'campaignref' => '805',
+	'passdata' => array(
+		'barcodevalue' => '1234',
+		'barcodetext' => '5678'
+	)
+);
+
+*/
+//-------saveMMS Test-------
+
+	$duration = 'slide duration="' . $_GET["duration"] .'"';
+
+	$request = array(
+		'action' => 'saveMMS',
+		'subject' => 'testMMS2',
+		'content' => array(
+			'name' => $_GET["name"],
+			'sequence' => array(
+				$duration => array(
+					'image' => array(
+						'url' => $_GET["url"]
+					),
+					'text' => 'Some Text'
+				),
+				'slide1' => array(
+					'image' => array(
+						'url' => $_GET["url"]
+					),
+					'text' => 'Some Text'
+				)
+			)
+		)
+	);
+
+
+//-------getEmailIds Test-------
+/*
+	$request = array(
+		'action' => 'getemailids'
+	);
+*/	
+
+//Build a skycore object
 $skycore = new Skycore($key);
-$skycore->subscribe($number, $campid);
+
+//Send a request to the object and get it's response
+$skycoreResponse = $skycore->makeAPI_Call($request);
+
+//Show the whol response
+//print_r ($skycoreResponse);
+
+//Access a piece of data in the response
+echo $skycoreResponse->STATUS;
+//echo $skycoreResponse->ERRORCODE;
+//echo $skycoreResponse->ERRORINFO;
+//echo $skycoreResponse->EMAILIDS;
 
 ?>
