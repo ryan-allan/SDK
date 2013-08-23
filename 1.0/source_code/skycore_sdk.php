7e3465e9c4d29c21408d29f39abf21e50b60c728
ryan-allan <ryan.allen@skycore.com>
2013-08-22 23:19:21 -0400

    Update skycore_sdk.php

--- 1.0/source_code/skycore_sdk.php
+++ 1.0/source_code/skycore_sdk.php
@@ -4,7 +4,8 @@
 Skycore SDK v1.0
 --------------------------------------------------
 This SDK will allow for implementation of the Skycore API via instantiation of a Skycore object and passing 
-an array containing the proper parameters to the makeAPI_Call function.
+an array containing the proper parameters to the makeAPI_Call function.  One can then assign the return value of this
+function to a an object and access any of the data from the response via this object.
 --------------------------------------------------
 A current list of the calls and their parameters can be found here at https://github.com/SkycoreMobile/API .
 --------------------------------------------------	
