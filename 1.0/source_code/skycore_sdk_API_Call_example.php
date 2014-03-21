f1d6378e001e29b84baea4ae45efc510139d6d9b
ryan-allan <ryan.allen@skycore.com>
2014-03-21 09:49:31 -0400

    Update skycore_sdk_API_Call_example.php

--- 1.0/source_code/skycore_sdk_API_Call_example.php
+++ 1.0/source_code/skycore_sdk_API_Call_example.php
@@ -1,48 +1,4 @@
 <?php
-/*
-Each form is matched with its respective array below.  You must de-comment the array to use it's respective form and 
-keep the others commented.  To use multiple forms you may change the name of the arrays to request1, request2 etc...
-and then send them to the skycore object 1 at a time.
-*/
-?>
-
-------------------------------------SUBSCRIBE FORM-----------------------------------------------------------<BR/>
-<form action="skycore_sdk_API_Call_example.php" method="get">
-
-Enter your phone number:<input type="text" name="mobile"><BR/>
-Format: 1-XXX-XXX-XXXX (excluding hyphens)<BR/>
-							  
-<input type="submit" name="submit" value="Subscribe">
-
-</form>
------------------------------------SEND PASS IN EMAIL FORM---------------------------------------------------<BR/>
-<form action="skycore_sdk_API_Call_example.php" method="get">
-
-Enter your Email:<input type="text" name="email"><BR/>
-Format: example@example.com<BR/>
-							  
-<input type="submit" name="submit" value="Send Pass To Email">
-
-</form>
--------------------------------------SAVE MMS FORM-----------------------------------------------------------<BR/>
-<form action="skycore_sdk_API_Call_example.php" method="get">
-
-Enter Name of the MMS:<input type="text" name="name"><BR/>
-<strong>Slide 1 Information:</strong><BR/>
-Enter Duration:<input type="text" name="duration1"><BR/>
-Enter Image URL:<input type="text" name="url1"><BR/>
-Enter Text:<input type="text" name="text1"><BR/>
-<strong>Slide 2 Information:</strong><BR/>
-Enter Duration:<input type="text" name="duration2"><BR/>
-Enter Image URL:<input type="text" name="url2"><BR/>
-Enter Text:<input type="text" name="text2"><BR/>
-							  
-<input type="submit" name="submit" value="Save MMS">
-
-</form>
--------------------------------------------------------------------------------------------------------------<BR/>
-
-<?php
 
 include ('skycore_sdk.php');
 	
